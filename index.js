51bd8f9b3e4f11a6418cb9148fccf3dcd432c109
linhlinh00 <mdothman79nam1311@outlook.com>
2024-05-14 13:22:46 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-function levelOrder(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    result.push(currentLevel);
-  }
-  return result;
+function sayHi() {
+  console.log("Hi!");
 }
+
+setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
